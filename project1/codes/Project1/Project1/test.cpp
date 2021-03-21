c15221509858132bc2c65cf45c7a12895618ee99
LukeLIN-web <1263810658@qq.com>
2021-03-21 21:56:15 +0800

    project1 finsihed

--- project1/codes/Project1/Project1/test.cpp
+++ project1/codes/Project1/Project1/test.cpp
@@ -6,51 +6,88 @@
 #include <opencv2/imgcodecs.hpp>
 using namespace cv;
 
-float Bicubic(float x);
-Mat ThreeInter(Mat &srcImage, double kx, double ky);
 Mat LinerInter(Mat &srcImage, double kx, double ky);
-Mat NearInter(Mat &srcImage, double kx, double ky)
-{
-	int rows = cvRound(srcImage.rows*kx);
-	int cols = cvRound(srcImage.cols*ky);
+Mat Downsampling(Mat &srcImage, double kx, double ky);
+Mat ThreeInter(Mat &srcImage);
+float a = -0.5;//BiCubic基函数
+void getW_x(float w_x[4], float x);
+void getW_y(float w_y[4], float y);
+Mat NearInter(Mat &srcImage, double kx, double ky){
+	int rows = cvRound(srcImage.rows*kx);// 0.6行, 高变为0.6
+	int cols = cvRound(srcImage.cols*ky);// 宽变为1.2
 	Mat resultImg(rows, cols, srcImage.type());//OpenCV2.0封装的一个C++类，用来表示一个图像，自动内存管理
 	int i, j, x, y;
 	for (i = 0; i < rows; i++){
-		x = static_cast<int>((i + 1) / kx + 0.5) - 1;
+		x = static_cast<int>((i + 1) / kx + 0.5) - 1;// static_cast<int>(i / kx + 0.5)  会出错我也不知道为啥
 		for (j = 0; j < cols; j++){
-			y = static_cast<int>((j + 1) / ky + 0.5) - 1;
-			resultImg.at<Vec3b>(i, j) = srcImage.at<Vec3b>(x, y);
+			y = static_cast<int>((j + 1) / ky + 0.5) - 1;//
+			resultImg.at<Vec3b>(i, j) = srcImage.at<Vec3b>(x, y);//目标各像素点的灰度值代替源图像中与其最邻近像素的灰度值。
 		}
 	}
 	return resultImg;
 }
-int main()
-{
-	// 读入一张图片（poyanghu缩小图）    
+int main(){
 	Mat dest1, dest2;
 	Mat srcImg = imread("Lina.jpg");//Opencv默认将读入图像强制转换为一幅三通道彩色图像，
-	// 创建一个名为 "图片"窗口    
-	namedWindow("图片");//第一个参数表示新窗口的名称，显示在窗口的顶部，同时用作highgui中其他函数调用该窗口的句柄。
-	// 在窗口中显示图片   
-	imshow("图片", srcImg);
-	// 等待6000 ms后窗口自动关闭    
-	//namedWindow("降采样后1", CV_WINDOW_AUTOSIZE);
-	//imshow("降采样后1", dest1);
-	Mat nearImg = NearInter(srcImg, 0.6, 1.2);
-	Mat doubleImg = LinerInter(srcImg, 0.6, 1.2);
-	Mat ThreeInterImg = ThreeInter(srcImg, 0.6, 1.2);
+	namedWindow("图片");// 创建一个名为 "图片"窗口 ,第一个参数表示新窗口的名称，显示在窗口的顶部，同时用作highgui中其他函数调用该窗口的句柄。
+	imshow("图片", srcImg);// 在窗口中显示图片   
+	dest1 = Downsampling(srcImg, 0.5, 0.5);
+	namedWindow("降采样后1", CV_WINDOW_AUTOSIZE);
+	imshow("降采样后1", dest1);
+	Mat nearImg = NearInter(dest1, 2, 2);
+	Mat doubleImg = LinerInter(dest1, 2, 2);
+	Mat ThreeInterImg = ThreeInter(dest1);
 	imshow("nearImg", nearImg);
-	imshow("resultImg", ThreeInterImg);
 	imshow("doubleImg", doubleImg);
-	waitKey(0);
+	imshow("ThreeInterImg", ThreeInterImg);
+	waitKey(0); //不消失
 	return 0;
 }
 
+Mat ThreeInter(Mat &image) {
+	float Row_B = image.rows * 2;
+	float Col_B = image.cols * 2;
 
+	Mat biggerImage(Row_B, Col_B, CV_8UC3);
 
+	for (int i = 2; i < Row_B - 4; i++) {
+		for (int j = 2; j < Col_B - 4; j++) {
+			float x = i * (image.rows / Row_B);//放大后的图像的像素位置相对于源图像的位置
+			float y = j * (image.cols / Col_B);//得到B（X,Y）在图像A中对应的位置（x,y）=(X*(m/M),Y*(N/n))
 
-Mat LinerInter(Mat &srcImage, double kx, double ky)
-{
+			/*if (int(x) > 0 && int(x) < image.rows - 2 && int(y)>0 && int(y) < image.cols - 2){*/
+			float w_x[4], w_y[4];//行列方向的加权系数
+			getW_x(w_x, x);//利用所选择的基函数，求出对应的每个像素的权值
+			getW_y(w_y, y);
+
+			Vec3f temp = { 0, 0, 0 };
+			for (int s = 0; s <= 3; s++) {
+				for (int t = 0; t <= 3; t++) {
+					temp = temp + (Vec3f)(image.at<Vec3b>(int(x) + s - 1, int(y) + t - 1))*w_x[s] * w_y[t];
+				}
+			}
+			biggerImage.at<Vec3b>(i, j) = (Vec3b)temp;
+		}
+	}
+	return biggerImage;
+}
+
+Mat Downsampling(Mat &srcImage,double kx,double ky ) {
+	int rows = cvRound(srcImage.rows*kx);
+	int cols = cvRound(srcImage.cols*ky);
+	Mat resultImg(rows, cols, srcImage.type());//OpenCV2.0封装的一个C++类，用来表示一个图像，自动内存管理
+	int i, j, x, y;
+	for (i = 0; i < rows; i++) {
+		x = static_cast<int>((i + 1) / kx + 0.5) - 1;// x = 
+		for (j = 0; j < cols; j++) {
+			y = static_cast<int>((j + 1) / ky + 0.5) - 1;//
+			resultImg.at<Vec3b>(i, j) = srcImage.at<Vec3b>(x, y);//
+		}
+	}
+	return resultImg;
+}
+
+Mat LinerInter(Mat &srcImage, double kx, double ky){
 	int rows = cvRound(srcImage.rows*kx);
 	int cols = cvRound(srcImage.cols*ky);
 	Mat resultImg(rows, cols, srcImage.type());
@@ -78,13 +115,9 @@ Mat LinerInter(Mat &srcImage, double kx, double ky)
 			dy = ym - yi;
 			//判断边界
 			if (x11 > (srcImage.rows - 1))
-			{
 				x11 = xi - 1;
-			}
 			if (y11 > (srcImage.cols - 1))
-			{
 				y11 = yi - 1;
-			}
 			//bgr
 			resultImg.at<Vec3b>(i, j)[0] = (int)(srcImage.at<Vec3b>(xi, yi)[0] * (1 - dx)*(1 - dy)
 				+ srcImage.at<Vec3b>(x11, yi)[0] * dx*(1 - dy)
@@ -104,80 +137,31 @@ Mat LinerInter(Mat &srcImage, double kx, double ky)
 	return resultImg;
 }
 
-float Bicubic(float y)
-{
-	float x = abs(y);
-	float a = -0.5;
-	if (x <= 1.0)
-	{
-		return (a + 2)*pow(x, 3) - (a + 3)*pow(x, 2) + 1;
-	}
-	else if (x < 2.0)
-	{
-		return a * pow(x, 3) + 5 * a*pow(x, 2) - 4 * a;
-	}
-	else
-	{
-		return 0.0;
-	}
-}
 
-Mat ThreeInter(Mat &srcImage, double kx, double ky)
-{
-	int rows = cvRound(srcImage.rows * kx);
-	int cols = cvRound(srcImage.cols * ky);
-	Mat resultImg(rows, cols, srcImage.type());
-	int i, j;
-	int xm, ym;
-	int x0, y0, xi, yi, x1, y1, x2, y2;
-	float wx0, wy0, wxi, wyi, wx1, wy1, wx2, wy2;
-	float w00, w01, w02, w0i, w10, w11, w12, w1i, w20, w21, w22, w2i, wi0, wi1, wi2, wii;
-	for (i = 0; i < rows; i++)
-	{
-		xm = i / kx;
-		xi = (int)xm;
-		x0 = xi - 1;
-		x1 = xi + 1;
-		x2 = xi + 2;
-		wx0 = Bicubic(x0 - xm);
-		wxi = Bicubic(xi - xm);
-		wx1 = Bicubic(x1 - xm);
-		wx2 = Bicubic(x2 - xm);
-		for (j = 0; j < cols; j++)
-		{
-			ym = j / ky;
-			yi = (int)ym;
-			y0 = yi - 1;
-			y1 = yi + 1;
-			y2 = yi + 2;
-			wy0 = Bicubic(y0 - ym);
-			wyi = Bicubic(yi - ym);
-			wy1 = Bicubic(y1 - ym);
-			wy2 = Bicubic(y2 - ym);
-			w00 = wx0 * wy0;
-			w01 = wx0 * wy1;
-			w02 = wx0 * wy2;
-			w0i = wx0 * wyi;
-			w10 = wx1 * wy0;
-			w11 = wx1 * wy1;
-			w12 = wx1 * wy2;
-			w1i = wx1 * wyi;
-			w20 = wx2 * wy0;
-			w21 = wx2 * wy1;
-			w22 = wx2 * wy2;
-			w2i = wx2 * wyi;
-			wi0 = wxi * wy0;
-			wi1 = wxi * wy1;
-			wi2 = wxi * wy2;
-			wii = wxi * wyi;
-			if ((x0 >= 0) && (x2 < srcImage.rows) && (y0 >= 0) && (y2 < srcImage.cols))
-			{
-				resultImg.at<Vec3b>(i, j) = (srcImage.at<Vec3b>(x0, y0)*w00 + srcImage.at<Vec3b>(x0, y1)*w01 + srcImage.at<Vec3b>(x0, y2)*w02 + srcImage.at<Vec3b>(x0, yi)*w0i
-					+ srcImage.at<Vec3b>(x1, y0)*w10 + srcImage.at<Vec3b>(x1, y1)*w11 + srcImage.at<Vec3b>(x1, y2)*w12 + srcImage.at<Vec3b>(x1, yi)*w1i
-					+ srcImage.at<Vec3b>(x2, y0)*w20 + srcImage.at<Vec3b>(x2, y1)*w21 + srcImage.at<Vec3b>(x2, y2)*w22 + srcImage.at<Vec3b>(x2, yi)*w2i
-					+ srcImage.at<Vec3b>(xi, y0)*wi0 + srcImage.at<Vec3b>(xi, y1)*wi1 + srcImage.at<Vec3b>(xi, y2)*wi2 + srcImage.at<Vec3b>(xi, yi)*wii);
-			}
-		}
-	}
-	return resultImg;
+/*计算系数*/
+void getW_x(float w_x[4], float x) {
+	int X = (int)x;//取整数部分, 这样就找到一个整数点
+	float stemp_x[4];
+	stemp_x[0] = 1 + (x - X);
+	stemp_x[1] = x - X;
+	stemp_x[2] = 1 - (x - X);
+	stemp_x[3] = 2 - (x - X);
+	// 根据公式计算 系数
+	w_x[0] = a * abs(stemp_x[0] * stemp_x[0] * stemp_x[0]) - 5 * a*stemp_x[0] * stemp_x[0] + 8 * a*abs(stemp_x[0]) - 4 * a;// x<2
+	w_x[1] = (a + 2)*abs(stemp_x[1] * stemp_x[1] * stemp_x[1]) - (a + 3)*stemp_x[1] * stemp_x[1] + 1;// x< 1
+	w_x[2] = (a + 2)*abs(stemp_x[2] * stemp_x[2] * stemp_x[2]) - (a + 3)*stemp_x[2] * stemp_x[2] + 1;// x< 1
+	w_x[3] = a * abs(stemp_x[3] * stemp_x[3] * stemp_x[3]) - 5 * a*stemp_x[3] * stemp_x[3] + 8 * a*abs(stemp_x[3]) - 4 * a;// x<2
+}
+void getW_y(float w_y[4], float y) {
+	int Y = (int)y;
+	float stemp_y[4];
+	stemp_y[0] = 1.0 + (y - Y);
+	stemp_y[1] = y - Y;
+	stemp_y[2] = 1.0 - (y - Y);
+	stemp_y[3] = 2.0 - (y - Y);
+	// 根据公式计算 系数
+	w_y[0] = a * abs(stemp_y[0] * stemp_y[0] * stemp_y[0]) - 5 * a*stemp_y[0] * stemp_y[0] + 8 * a*abs(stemp_y[0]) - 4 * a;// x<2
+	w_y[1] = (a + 2)*abs(stemp_y[1] * stemp_y[1] * stemp_y[1]) - (a + 3)*stemp_y[1] * stemp_y[1] + 1;// x  <1
+	w_y[2] = (a + 2)*abs(stemp_y[2] * stemp_y[2] * stemp_y[2]) - (a + 3)*stemp_y[2] * stemp_y[2] + 1;// x< 1
+	w_y[3] = a * abs(stemp_y[3] * stemp_y[3] * stemp_y[3]) - 5 * a*stemp_y[3] * stemp_y[3] + 8 * a*abs(stemp_y[3]) - 4 * a; // x<2
 }
